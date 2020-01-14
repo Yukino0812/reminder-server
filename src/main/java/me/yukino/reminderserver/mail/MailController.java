83a09f8bfef7f853ad20c43d4c0564de797d0ebb
Yukino0812 <Yukino0812@qq.com>
2020-01-14 21:30:36 +0800

    Fix send mail a post api

--- src/main/java/me/yukino/reminderserver/mail/MailController.java
+++ src/main/java/me/yukino/reminderserver/mail/MailController.java
@@ -2,10 +2,7 @@ package me.yukino.reminderserver.mail;
 
 import me.yukino.reminderserver.vo.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Yukino Yukinoshita
@@ -22,7 +19,7 @@ public class MailController {
         this.mailService = mailService;
     }
 
-    @RequestMapping("/send")
+    @PostMapping("/send")
     public ResponseResult sendMail(@RequestHeader("auth_key") String key,
                                    @RequestParam String address,
                                    @RequestParam String subject,
